a58a6e5cfcc6b4bd428276c91efa37be837da600
Soden46 <syarifsoden22@gmail.com>
2024-03-10 18:20:04 +0700

    Update

--- resources/views/soden/index.blade.php
+++ resources/views/soden/index.blade.php
@@ -281,10 +281,10 @@
     <!-- JavaScript Libraries -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.4/jquery.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.bundle.min.js"></script>
-    <script src="../wow/wow.min.js')}}"></script>
-    <script src="../easing/easing.min.js')}}"></script>
-    <script src="../waypoints/waypoints.min.js')}}"></script>
-    <script src="../owlcarousel/owl.carousel.min.js')}}"></script>
+    <script src="../wow/wow.min.js"></script>
+    <script src="../easing/easing.min.js"></script>
+    <script src="../waypoints/waypoints.min.js"></script>
+    <script src="../owlcarousel/owl.carousel.min.js"></script>
 
     <!-- Template Javascript -->
     <script src="../main.js"></script>
